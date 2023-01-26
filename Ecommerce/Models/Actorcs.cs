e36ad5e996c8e2dd990c3f602b1c8b053cd96f32
Vimalviva <vimalviva555@gmail.com>
2023-01-27 00:09:25 +0530

    Models added

---
+++ Ecommerce/Models/Actorcs.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Models
+{
+    public class Actorcs : Person
+    {
+        [Key]
+        public int Id { get; set; }
+        public string propic { get; set; }
+        public string Name { get; set; }
+        public string Bio { get; set; }
+
+    }
+}
