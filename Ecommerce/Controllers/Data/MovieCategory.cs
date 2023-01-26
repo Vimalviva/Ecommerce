c1599203536f67d1da3988a5c69fe06ad748452c
Vimalviva <vimalviva555@gmail.com>
2023-01-27 00:32:34 +0530

    models added

---
+++ Ecommerce/Controllers/Data/MovieCategory.cs
@@ -0,0 +1,14 @@
+﻿namespace Ecommerce.Controllers.Data
+{
+    public enum MovieCategory
+    {
+        Action=1,
+        Romance,
+        Thriller,
+        Sitcom,
+        Sciencefiction,
+        Fantasy,
+        Documentary
+
+    }
+}
